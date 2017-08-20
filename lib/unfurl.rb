04342bc603cc346a3a1be1b424c49408ac0b8396
Michael Seneadza <mseneadza@alumni.stanford.edu>
2017-08-20 16:56:01 -0400

    adds functional unfurling

--- lib/unfurl.rb
+++ lib/unfurl.rb
@@ -1,5 +1,27 @@
-require "unfurl/version"
+require 'unfurl/version'
 
 class Unfurl
-  # Your code goes here...
+  def initialize(input_matrix)
+    @matrix = input_matrix.dup # duplicating to prevent mutating the passed in matrix
+  end
+
+  def process
+    #validate input
+    result = unfurl(@matrix)
+    print(result)
+  end
+
+  def unfurl(matrix)
+    result = []
+    result << matrix.slice!(0)
+    unless matrix.empty?
+      rotated_matrix = MatrixTurner.new(matrix).turn
+      result << unfurl(rotated_matrix)
+    end
+    result.flatten
+  end
+
+  def print(unfurled_matrix)
+    unfurled_matrix.map(&:downcase).join(' ')
+  end
 end
