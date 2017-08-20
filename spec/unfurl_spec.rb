7ec0482d1b69f347e718568e6d470fb2aa0b5772
Michael Seneadza <mseneadza@alumni.stanford.edu>
2017-08-20 16:43:22 -0400

    fill in spec

--- spec/unfurl_spec.rb
+++ spec/unfurl_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe Unfurl do
     expect(Unfurl::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "can unfurl a small matrix" do
+    # make these expect arrays
+    input = [['A', 'B'],
+             ['C', 'D']]
+    expect(Unfurl.new(nil).unfurl(input)).to eq(%w(A B D C))
+  end
+
+  it "can unfurl a large matrix" do
+    input = [['A', 'B', 'C', 'D'],
+             ['E', 'F', 'G', 'H'],
+             ['I', 'J', 'K', 'L']]
+    expect(Unfurl.new(nil).unfurl(input)).to eq(%w(A B C D H L K J I E F G))
+  end
+
+  it 'can properly format its output' do
+    # lower case and space it
   end
 end
